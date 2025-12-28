7f5e0e8caf214c9bbe97f5148e0b7660c690ba68
rom efroni <efronii14@gmail.com>
2025-12-28 17:24:26 +0200

    hw7

--- Koch.java
+++ Koch.java
@@ -8,14 +8,14 @@ public class Koch {
 		//// Uncomment only one block in each test, and remember to compile
 		//// the class whenever you change the test.
 
-        /*
+        
 		// Tests the curve function:
 		// Gets n, x1, y1, x2, y2,
-		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
+		//and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
 		curve(Integer.parseInt(args[0]),
-			  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
+			  Double.parseDouble(args[1]), Double.parseDouble(args[2]),
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-		*/
+		
 
 		/*
 		// Tests the snowflake function:
@@ -27,8 +27,30 @@ public class Koch {
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		if (n == 0) {
+            
+            StdDraw.line(x1, y1, x2, y2);
+        } else {
+           
+            double dx = (x2 - x1) / 3.0;
+            double dy = (y2 - y1) / 3.0;
+
+            double xA = x1 + dx;
+            double yA = y1 + dy;
+
+            double xB = x1 + 2 * dx;
+            double yB = y1 + 2 * dy;
+
+           
+            double xC = (x1 + x2) / 2.0 - Math.sqrt(3) * (y2 - y1) / 6.0;
+            double yC = (y1 + y2) / 2.0 + Math.sqrt(3) * (x2 - x1) / 6.0;
+
+            
+            curve(n - 1, x1, y1, xA, yA);
+            curve(n - 1, xA, yA, xC, yC);
+            curve(n - 1, xC, yC, xB, yB);
+            curve(n - 1, xB, yB, x2, y2);
+        }
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
@@ -36,7 +58,17 @@ public class Koch {
 		// A little tweak that makes the drawing look better
 		StdDraw.setYscale(0, 1.1);
 		StdDraw.setXscale(0, 1.1);
-		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		
+		double x1 = 0.1, y1 = 0.1;
+        double x2 = 1.0, y2 = 0.1;
+
+        double height = Math.sqrt(3) / 2 * (x2 - x1);
+        double x3 = (x1 + x2) / 2.0;
+        double y3 = y1 + height;
+
+        
+        curve(n, x1, y1, x2, y2);
+        curve(n, x2, y2, x3, y3);
+        curve(n, x3, y3, x1, y1);
 	}
 }
